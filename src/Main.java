2227ec11e4144f2d9d678635558c0f2f06f11c9a
Manasi670430 <manasijoshi1993@gmail.com>
2018-12-24 00:38:04 -0800

    Including variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import japa.parser.JavaParser;
 import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.body.BodyDeclaration;
+import japa.parser.ast.body.FieldDeclaration;
+import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.body.TypeDeclaration;
+import japa.parser.ast.type.Type;
 import net.sourceforge.plantuml.SourceStringReader;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Modifier;
+import java.util.List;
 
 public class Main {
 
@@ -18,40 +25,79 @@ public class Main {
                 //System.out.println(F.getCanonicalPath());
                 CompilationUnit cu = JavaParser.parse(F);
                 //System.out.println(cu.toString());
-                b += "class " +cu.getTypes().get(0).getName() + "\n";
-                System.out.println(b + "\n");
+
+                b += "class " +cu.getTypes().get(0).getName()+ " { " + "\n" +accessMembers(cu.getTypes().get(0))+ " } "  + "\n";
+                //System.out.println("dadddsdsds" +b );
                 //System.out.println();
 
             }
         }
 
-
-
         generateUML(b);
-
-
     }
 
+
+    private static String accessMembers(TypeDeclaration td){
+        String am="";
+        List<BodyDeclaration> members = td.getMembers();
+        for(BodyDeclaration m : members){
+            if(m.getClass().equals(FieldDeclaration.class)) {
+                FieldDeclaration fd =(FieldDeclaration)m;
+                if(fd.getModifiers()== Modifier.PUBLIC){
+                    am += "+ " ;
+                }
+                else if(fd.getModifiers()== Modifier.PRIVATE){
+                    am += "- " ;
+                }
+                else if(fd.getModifiers()== Modifier.PROTECTED){
+                    am += "# " ;
+                }
+
+                am += fd.getVariables().get(0).toString()  ;
+
+                am+=" : " + fd.getType().toString() + "\n";
+            } else if(m.getClass().equals(MethodDeclaration.class)){
+
+            }
+        }
+
+        //String s="";
+        return am;
+    }
 
 
     private static void generateUML(String b) throws Exception {
         OutputStream png = new FileOutputStream("output.png");
 
-        System.out.println(b);
+        //System.out.println(b);
 
         String source = "@startuml\n";
         source += b;
         source += "@enduml\n";
         System.out.println(source);
 
-//
+//        b="@startuml\n" +
+//                "class A {\n" +
+//                "-x\n" +
+//                "-y\n" +
+//                "}\n" +
+//                "class B {\n" +
+//                "}\n" +
+//                "class C {\n" +
+//                "}\n" +
+//                "class D {\n" +
+//                "}\n" +
+//                "@enduml";
+
+        //System.out.println(b);
+
+
 //        String source = "@startuml\n";
 //        source += "class ClassA {\n" +
 //            source +=   "\tint id\n " +
 //            source +=    "}\n" +
 //        source += "@enduml\n";
 
-                System.out.println(source);
 
         //        String source = "@startuml\n";
 //        source += "Bob -> Alice : hello\n";
